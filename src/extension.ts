3e6c5c0ecc75b9d3dcbe48ae0ab2944c20144e63
spoonk <cornwlucas@gmail.com>
2024-06-28 16:28:29 -0700

    refactor mostly done, but now bugs have appeared

--- src/extension.ts
+++ src/extension.ts
@@ -1,122 +1,29 @@
 import * as vscode from "vscode";
 import { QuickPickItem } from "vscode";
-
-class TabGroupContext {
-  groups: Group[];
-  currentGroupNum: number;
-  statusBarItems: vscode.StatusBarItem[];
-
-  constructor() {
-    this.groups = [];
-    this.currentGroupNum = -1;
-    this.statusBarItems = [];
-  }
-
-  getGroups() {
-    return this.groups;
-  }
-
-  addGroup(editors: readonly vscode.TextEditor[]) {
-    this.groups.push({
-      items: editors.map((editor) => {
-        return {
-          document: editor.document,
-          viewColumn: editor.viewColumn,
-        };
-      }),
-    });
-  }
-}
-
-type Group = {
-  items: GroupItemState[];
-};
-
-type GroupItemState = {
-  readonly document: vscode.TextDocument;
-  viewColumn: vscode.ViewColumn | undefined;
-};
-
-class TabGroupPickItem implements QuickPickItem {
-  label: string;
-  description?: string | undefined;
-
-  constructor(public name: string, public path: string, public group: Group) {
-    this.label = name;
-    this.description = group.items
-      .map((item) => item.document.fileName.replace(/^.*[\\/]/, ""))
-      .join(" | ");
-    this.group = group;
-  }
-}
-
-const tabContext = new TabGroupContext();
+import {
+  TabGroupContextManager,
+  TabGroupPickItem,
+} from "./tab-context-manager";
+const tabContext = new TabGroupContextManager();
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  const disposable = vscode.commands.registerCommand("tabs.helloWorld", () => {
-    vscode.window.showInformationMessage("raaaaa!!!");
-    vscode.window.showInformationMessage("hello");
-  });
-
   const tabInfo = vscode.commands.registerCommand("tabs.tabInfo", () => {
     console.log(tabContext.getGroups());
   });
 
   const addGroup = vscode.commands.registerCommand("tabs.addGroup", () => {
     const visibleEditors = vscode.window.visibleTextEditors;
-    if (tabContext.groups.length >= 5) {
+    if (tabContext.numGroups() >= 5) {
+      // @todo: in the future support more and shorten the list
       vscode.window.showErrorMessage("Cannot add more than 5 tab groups");
       return;
     }
 
     tabContext.addGroup(visibleEditors);
-    // set status bar text as not active for the group that is being
-    // deactivated
-    if (tabContext.groups.length > 1) {
-      tabContext.statusBarItems[
-        tabContext.currentGroupNum
-      ].text = ` ${tabContext.currentGroupNum.toString()} `;
-    }
-    tabContext.currentGroupNum = tabContext.groups.length - 1; // current group is last group
-    const statusBarItem = vscode.window.createStatusBarItem(
-      vscode.StatusBarAlignment.Left,
-      100000
-    );
-
-    statusBarItem.name = tabContext.currentGroupNum.toString();
-    statusBarItem.command = `tabs.pickGroup${tabContext.currentGroupNum}`;
-    statusBarItem.text = `[${tabContext.currentGroupNum.toString()}]`;
-    statusBarItem.tooltip = `${tabContext.currentGroupNum}`;
-
-    statusBarItem.tooltip = visibleEditors
-      .map((item) => item.document.fileName.replace(/^.*[\\/]/, ""))
-      .join("\n");
-
-    statusBarItem.show();
-    tabContext.statusBarItems.push(statusBarItem);
     vscode.window.showInformationMessage("added new group");
   });
-
-  const closeCurrentEditors = async (
-    newGroupViewColumns: (vscode.ViewColumn | undefined)[]
-  ) => {
-    const currentTabs = vscode.window.tabGroups.all;
-    const tabsToClose: vscode.TabGroup[] = [];
-    for (const group of currentTabs) {
-      if (newGroupViewColumns.includes(group.viewColumn)) {
-        continue;
-      }
-      tabsToClose.push(group);
-    }
-
-    try {
-      await vscode.window.tabGroups.close(tabsToClose);
-    } catch (error) {
-      console.warn(`ERROR: ${error}`);
-    }
-  };
 
   const pickGroup = vscode.commands.registerCommand(
     "tabs.pickGroup",
@@ -133,68 +40,46 @@ export function activate(context: vscode.ExtensionContext) {
       );
 
       if (item instanceof TabGroupPickItem) {
-        loadTabGroup(Number(item.label));
+        tabContext.loadTabGroup(Number(item.label));
       } else {
         vscode.window.showInformationMessage("raa");
       }
     }
   );
-
-  const loadTabGroup = async (groupNum: number) => {
-    const group = tabContext.getGroups()[groupNum];
-
-    await closeCurrentEditors(group.items.map((i) => i.viewColumn));
-    for (const groupItem of group.items) {
-      vscode.window.showTextDocument(groupItem.document, {
-        viewColumn: groupItem.viewColumn,
-      });
-    }
-
-    tabContext.statusBarItems[
-      tabContext.currentGroupNum
-    ].text = ` ${tabContext.currentGroupNum.toString()} `;
-
-    // set as active
-    tabContext.statusBarItems[groupNum].text = `[${groupNum}]`;
-
-    tabContext.currentGroupNum = groupNum;
-    vscode.window.showInformationMessage(`swapped to group ${groupNum}`);
-  };
-
   const nextGroup = vscode.commands.registerCommand(
     "tabs.nextGroup",
     async () => {
       const groupNumToLoad =
-        (tabContext.currentGroupNum + 1) % tabContext.groups.length;
-      await loadTabGroup(groupNumToLoad);
+        (tabContext.currentGroupNum() + 1) % tabContext.numGroups();
+      await tabContext.loadTabGroup(groupNumToLoad);
     }
   );
 
   const previousGroup = vscode.commands.registerCommand(
     "tabs.previousGroup",
     async () => {
-      let groupNumToLoad = tabContext.currentGroupNum - 1;
+      let groupNumToLoad = tabContext.currentGroupNum() - 1;
       if (groupNumToLoad < 0) {
         // wrap back around
-        groupNumToLoad = tabContext.groups.length - 1;
+        groupNumToLoad = tabContext.numGroups() - 1;
       }
 
-      await loadTabGroup(groupNumToLoad);
+      await tabContext.loadTabGroup(groupNumToLoad);
     }
   );
 
   const pickGroupHelper = async (desiredGroupNum: number) => {
-    if (tabContext.currentGroupNum == desiredGroupNum) {
+    if (tabContext.currentGroupNum() == desiredGroupNum) {
       vscode.window.showWarningMessage(
         `Group ${desiredGroupNum} already selected`
       );
       return;
-    } else if (tabContext.groups.length <= desiredGroupNum) {
+    } else if (tabContext.numGroups() <= desiredGroupNum) {
       vscode.window.showErrorMessage(`Group ${desiredGroupNum} doesn't exist`);
       return;
     }
 
-    loadTabGroup(desiredGroupNum);
+    tabContext.loadTabGroup(desiredGroupNum);
   };
 
   for (const num of [0, 1, 2, 3, 4, 5]) {
@@ -206,7 +91,7 @@ export function activate(context: vscode.ExtensionContext) {
     // @todo: register 5 deleters
   }
 
-  context.subscriptions.push(disposable);
+  // context.subscriptions.push(disposable);
   context.subscriptions.push(tabInfo);
   context.subscriptions.push(addGroup);
   context.subscriptions.push(pickGroup);
---
+++ src/tab-context-manager.ts
@@ -0,0 +1,148 @@
+import * as vscode from "vscode";
+import { QuickPickItem } from "vscode";
+
+// refactor todo list
+// - don't access fields directly
+// - extract common checks on fields into functions with clear names
+
+export class TabGroupContextManager {
+  private groups: Group[];
+  private currGroupNum: number;
+  //   private statusBarItems: vscode.StatusBarItem[];
+
+  // @todo: unify groups and statusBarItems, since they will always occur together
+  constructor() {
+    this.groups = [];
+    this.currGroupNum = -1;
+  }
+
+  getGroups() {
+    return this.groups;
+  }
+
+  numGroups(): number {
+    return this.groups.length;
+  }
+
+  currentGroupNum(): number {
+    return this.currGroupNum;
+  }
+
+  updateStatusBarItemText(text: string, itemIndex: number): void {
+    if (
+      itemIndex < 0 ||
+      itemIndex >= this.numGroups() ||
+      this.currGroupNum == -1
+    ) {
+      return;
+    }
+
+    this.groups[this.currGroupNum].statusBarItem.text = text;
+  }
+
+  private initializeNewStatusBarItem(editors: readonly vscode.TextEditor[]) {
+    const statusBarItem = vscode.window.createStatusBarItem(
+      vscode.StatusBarAlignment.Left,
+      100000
+    );
+
+    statusBarItem.name = this.currentGroupNum().toString();
+    statusBarItem.command = `tabs.pickGroup${this.currentGroupNum()}`;
+    statusBarItem.text = `[${this.currentGroupNum().toString()}]`;
+    statusBarItem.tooltip = `${this.currentGroupNum()}`;
+
+    statusBarItem.tooltip = editors
+      .map((item) => item.document.fileName.replace(/^.*[\\/]/, ""))
+      .join("\n");
+
+    statusBarItem.show();
+    return statusBarItem;
+  }
+
+  async unfocusGroup(groupNum: number) {
+    await this.closeCurrentEditors(
+      this.groups[groupNum].items.map((i) => i.viewColumn)
+    );
+    // sets the text of status bar item to be unfocused variant
+    this.updateStatusBarItemText(` ${groupNum} `, groupNum);
+  }
+
+  addGroup(editors: readonly vscode.TextEditor[]) {
+    if (this.numGroups() >= 0) {
+      this.unfocusGroup(this.currGroupNum);
+    }
+    this.currGroupNum += 1;
+
+    // no need to focus group when adding, since it will definitely already be focused
+
+    const statusBarItem = this.initializeNewStatusBarItem(editors);
+    this.groups.push({
+      items: editors.map((editor) => {
+        return {
+          document: editor.document,
+          viewColumn: editor.viewColumn,
+        };
+      }),
+      statusBarItem,
+    });
+  }
+
+  closeCurrentEditors = async (
+    newGroupViewColumns: (vscode.ViewColumn | undefined)[]
+  ) => {
+    const currentTabs = vscode.window.tabGroups.all;
+    const tabsToClose: vscode.TabGroup[] = [];
+    for (const group of currentTabs) {
+      if (newGroupViewColumns.includes(group.viewColumn)) {
+        continue;
+      }
+      tabsToClose.push(group);
+    }
+
+    try {
+      await vscode.window.tabGroups.close(tabsToClose);
+    } catch (error) {
+      console.warn(`ERROR: ${error}`);
+    }
+  };
+
+  focusGroup(groupNum: number) {
+    const group = this.groups[groupNum];
+    for (const groupItem of group.items) {
+      vscode.window.showTextDocument(groupItem.document, {
+        viewColumn: groupItem.viewColumn,
+      });
+    }
+    this.currGroupNum = groupNum;
+    this.updateStatusBarItemText(`[${groupNum}]`, groupNum);
+  }
+
+  async loadTabGroup(groupNum: number) {
+    this.unfocusGroup(this.currGroupNum);
+    this.focusGroup(groupNum);
+    vscode.window.showInformationMessage(`swapped to group ${groupNum}`);
+  }
+}
+
+type Group = {
+  items: readonly GroupItemState[];
+  statusBarItem: vscode.StatusBarItem;
+};
+
+type GroupItemState = {
+  readonly document: vscode.TextDocument;
+  viewColumn: vscode.ViewColumn | undefined;
+};
+
+export class TabGroupPickItem implements QuickPickItem {
+  label: string;
+  description?: string | undefined;
+
+  constructor(public name: string, public path: string, public group: Group) {
+    this.label = name;
+    this.description = group.items
+      .map((item) => item.document.fileName.replace(/^.*[\\/]/, ""))
+      .join(" | ");
+    this.group = group;
+  }
+}
